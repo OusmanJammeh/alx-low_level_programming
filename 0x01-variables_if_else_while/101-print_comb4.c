1b88188e993a56ddcf966acf27b90229d04dbb60
Ousman Jammeh <ousmanjammeh09@gmail.com>
2022-12-11 07:10:42 -0800

    The success combination in business is Do what you do better and do more of what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,35 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
+ * main - prints all possible different combination of three digits,
+ *        in ascending order, seperated by a comma followed by a space.
  *
- * Return: 0
+ * Return: Always 0
  */
-
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
-	int hundreds = '0';
+	int digit1, digit2, digit3;
 
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (tens = '0'; tens <= '9'; tens++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (ones = '0'; ones <= '9'; ones++)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				if (!((ones == tens) || (tens == hundreds) || (tenss > ones) || (hundreds > tens)))
-					/* eliminates repitition */
-				{
-					putchar(hundreds);
-					putchar(tens);
-					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' && tens == '8')) /* adds commas and spaces */
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
+}
