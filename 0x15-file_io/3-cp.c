2ceea2c3431d6e5f8cc95ff158bc3d914abb7158
Ousman Jammeh <ousmanjammeh09@gmail.com>
2023-02-07 15:22:36 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILEN0,
+			dprintf(STDERR_FILENO,
 					"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
